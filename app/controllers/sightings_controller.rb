9a3b27ff2e8ae755930d35e6dd5cc5055375f051
Carley Tripp <carley.tripp@outlook.com>
2020-04-13 21:59:01 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,7 +3,7 @@ class SightingsController < ApplicationController
     @sightings = Sighting.all
     render json: SightingSerializer(@sightings).to_serialized_json
   end
-  
+
   def show
       @sighting = Sighting.find(params[:id])
       render json: SightingSerializer(@sighting).to_serialized_json
