1cb58e06346dd84723f0147ca08d0afda733adf0
Carley Tripp <carley.tripp@outlook.com>
2020-04-13 21:58:58 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,11 @@
 class SightingsController < ApplicationController
-    def show
-        @sighting = Sighting.find(params[:id])
-        render json: SightingSerializer(@sighting).to_serialized_json
-    end
+  def index
+    @sightings = Sighting.all
+    render json: SightingSerializer(@sightings).to_serialized_json
+  end
+  
+  def show
+      @sighting = Sighting.find(params[:id])
+      render json: SightingSerializer(@sighting).to_serialized_json
+  end
 end
